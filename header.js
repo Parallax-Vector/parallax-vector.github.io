d72da0f55272395e6ce36e6a13618f3d0a29685a
TheMiner63 <cboddy1@hotmail.com>
2016-11-20 16:27:08 +0000

    and again

--- header.js
+++ header.js
@@ -19,5 +19,5 @@ var blog =  document.getElementById('blog');
 var fountain =  document.getElementById('fountain');
 var didcot =  document.getElementById('didcot');
 
-console.log(domain.getAttribute("href"));
-console.log(beacon);
+console.log(domain);
+console.log(beacon.getAttribute("href"));
