861fd929c797fedeb0f95d392a976abeeefe609e
TheMiner63 <cboddy1@hotmail.com>
2016-11-20 15:33:53 +0000

    update 2

---
+++ header.js
@@ -0,0 +1,22 @@
+var domain = window.location.host;
+
+var home =  document.getElementById('home')
+var about =  document.getElementById('about')
+var leaders =  document.getElementById('our_leaders')
+var mission =  document.getElementById('mission')
+var church_life =  document.getElementById('church_life')
+var mornings =  document.getElementById('sunday_mornings')
+var children =  document.getElementById('children')
+var events =  document.getElementById('recent_events')
+var ministries =  document.getElementById('ministries')
+var beacon =  document.getElementById('beacon')
+var cap =  document.getElementById('cap')
+var tsm =  document.getElementById('tsm')
+var contact =  document.getElementById('contact')
+var media =  document.getElementById('media')
+var sermons =  document.getElementById('sermons')
+var blog =  document.getElementById('blog')
+var fountain =  document.getElementById('fountain')
+var didcot =  document.getElementById('didcot')
+
+console.log(domain)
