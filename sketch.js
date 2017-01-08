29a5cf743d27a31a217a0d04630048231c8f4ab1
TheMiner63 <cboddy1@hotmail.com>
2017-01-08 15:05:11 +0000

    Added sketch 4 and polished sketch 1
    Cleared up mp3 files and removed audio sourcing program. Credit to
    Daniel Shiffman with original code for sketch 1. The code is no longer
    identical and at least 75% is modified.

--- sketch.js
+++ sketch.js
@@ -9,10 +9,15 @@ var fft;
 var amplitude;
 //Canvas Visual Properties
 var margin = 10;
+var scroll = 0;
+var offset = 0;
+var progressLW = 4;
 //Audio Properties
+var historyLength = 64;
 var volumeHistory = [];
-var ampRecord = 0;
-var beatInterval = 0;
+var bassHistory = [];
+var midHistory = [];
+var trebleHistory = [];
 //Buttons
 var pauseButton;
 var restartButton;
@@ -27,11 +32,13 @@ function toggleSong() {
 
 function restartSong() {
     song.stop();
+    ampRecord = 0;
+    volumeHistory = [0];
     song.play();
 }
 
 function preload() {
-    song = loadSound('LHPKOy3dtqQ.mp3');
+    song = loadSound('sample.mp3');
 }
 
 function setup() {
@@ -48,10 +55,22 @@ function setup() {
 }
 
 function draw() {
+    if (scroll != 0) {
+        if (scroll > 0) {
+            offset += scroll;
+            scroll -= 1;
+        } else if (scroll < 0) {
+            offset += scroll;
+            scroll += 1;
+        }
+    }
+    translate(0, -offset);
     background(0);
     drawAmplitude(width/2, 0, width, height/2);
     drawSpiral(0, 0, width/2, height/2);
-    drawBars(0, height/2, width/2, height);
+    drawBars(0, height / 2, width / 2, height);
+    drawDissection(width / 2, height / 2, width, height);
+    //console.log(song.currentTime());
   }
 
 function drawSpiral(left, top, right, bottom) {
@@ -76,7 +95,7 @@ function drawSpiral(left, top, right, bottom) {
         var r = map(amp, 0, 256, 20, radius);
         var x = r * cos(angle);
         var y = r * sin(angle);
-        stroke(i, 255, 255);
+        stroke((360/spectrum.length)*i, 255, 255);
         line(0, 0, x, y);
     }
     pop();
@@ -113,25 +132,92 @@ function drawAmplitude(left, top, right, bottom) {
     volumeHistory.pop();
     volumeHistory.reverse();
     volumeHistory.push(soundLevel);
-    while (volumeHistory.length < 64) {
+    while (volumeHistory.length < historyLength) {
         volumeHistory.push(0);
     }
     //Visual Representation
     colorMode(RGB);
-    if (volumeHistory[volumeHistory.length - 1] > ampRecord) {
-        ampRecord = volumeHistory[volumeHistory.length - 1];
-    }
+    stroke(255);
     strokeWeight(graphWidth / volumeHistory.length - 2);
     for (i = 0; i < volumeHistory.length; i++) {
-        if (volumeHistory[i] >= ampRecord) {
-            stroke(255, 0, 0);
-        } else {
-            stroke(255);
-        }
         line(i * (graphWidth / volumeHistory.length), -volumeHistory[i], i * (graphWidth / volumeHistory.length), volumeHistory[i]);
     }
-    strokeWeight((graphWidth / volumeHistory.length - 2) / 2);
     stroke(255, 0, 0);
+    progressLW = (graphWidth / volumeHistory.length - 2) / 2;
+    strokeWeight(progressLW);
     line(graphWidth, -graphHeight / 2, graphWidth, graphHeight / 2);
     pop();
+}
+
+function drawDissection(left, top, right, bottom) {
+    //Positioning
+    push();
+    var graphWidth = right - left - (margin * 2);
+    var graphHeight = bottom - top - (margin * 2);
+    translate(left + margin, bottom - margin);
+    //Audio Processing
+    var spectrum = fft.analyze();
+    var bass = subset(spectrum, 0, int(spectrum.length / 3));
+    var mid = subset(spectrum, int(spectrum.length /3)+1, int(spectrum.length / 3));
+    var treble = subset(spectrum, int(spectrum.length / 3) * 2, int(spectrum.length / 3));
+    var bassLvl = 0; var midLvl = 0; var trebleLvl = 0;
+    for (freq = 0; freq < bass.length; freq++) {
+        bassLvl += bass[freq];
+    }
+    for (freq = 0; freq < mid.length; freq++) {
+        midLvl += mid[freq];
+    }
+    for (freq = 0; freq < treble.length; freq++) {
+        trebleLvl += treble[freq];
+    }
+    bassLvl = bassLvl / bass.length; midLvl = midLvl / mid.length; trebleLvl = trebleLvl / treble.length;
+    //console.log(bassLvl, midLvl, trebleLvl);
+    bassHistory.reverse();
+    bassHistory.pop();
+    bassHistory.reverse();
+    bassHistory.push(bassLvl);
+    while (bassHistory.length < historyLength) {
+        bassHistory.push(0);
+    }
+    midHistory.reverse();
+    midHistory.pop();
+    midHistory.reverse();
+    midHistory.push(midLvl);
+    while (midHistory.length < historyLength) {
+        midHistory.push(0);
+    }
+    trebleHistory.reverse();
+    trebleHistory.pop();
+    trebleHistory.reverse();
+    trebleHistory.push(trebleLvl);
+    while (trebleHistory.length < historyLength) {
+        trebleHistory.push(0);
+    }
+    //Visual Representation
+    colorMode(RGB);
+    strokeWeight(2);
+    stroke(255, 0, 0);
+    for (i = 0; i < bassHistory.length; i++) {
+        line(i * (graphWidth/bassHistory.length),-bassHistory[i],(i+1)*(graphWidth/bassHistory.length),-bassHistory[i+1]);
+    }
+    stroke(0,0,255);
+    for (i = 0; i < trebleHistory.length; i++) {
+        line(i * (graphWidth / trebleHistory.length), -trebleHistory[i], (i + 1) * (graphWidth / trebleHistory.length), -trebleHistory[i + 1]);
+    }
+    stroke(0,255,0);
+    for (i = 0; i < midHistory.length; i++) {
+        line(i * (graphWidth / midHistory.length), -midHistory[i], (i + 1) * (graphWidth / midHistory.length), -midHistory[i + 1]);
+    }
+    strokeWeight(progressLW);
+    stroke(255);
+    line(graphWidth, -graphHeight, graphWidth, 0);
+}
+
+function mouseWheel(event) {
+    if (event.delta > 0) {
+        scroll = 10
+    } else if (event.delta < 0) {
+        scroll = -10
+    }
+    return false;
 }
